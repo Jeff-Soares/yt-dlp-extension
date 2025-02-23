2802f5cda8efbf292e783ea0aaa44e51fe8d197c
Jeff <jeffersonvsoares@hotmail.com>
2025-02-23 13:41:48 -0300

    add chrome extension

---
+++ chrome/background.js
@@ -0,0 +1,12 @@
+chrome.action.onClicked.addListener((tab) => {
+    const url = tab.url;
+    console.log(`URL: ${url}`);
+
+    chrome.runtime.sendNativeMessage("com.jx.yt_dlp", { "url": url }, (response) => {
+        if (chrome.runtime.lastError) {
+            console.error("Error: ", chrome.runtime.lastError);
+        } else {
+            console.log("Result: ", response);
+        }
+    });
+});
